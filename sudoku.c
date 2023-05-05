079a0f450464f543949fcfc1eda85deed7392cc6
Replit user <>
2023-05-05 12:47:27 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -153,6 +153,7 @@ int is_final(Node* n){
         {
             printf("%i",n->sudo[i][j]);
             if (n->sudo[i][j]==0)return 0;
+            printf("\n");
         }
         printf("\n");
     }
