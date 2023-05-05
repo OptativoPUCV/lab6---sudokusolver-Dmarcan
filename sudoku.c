aff8e6b9dc2a804b6bea19ee763d6a04ab90feac
Replit user <>
2023-05-05 03:02:59 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -147,13 +147,12 @@ List* get_adj_nodes(Node* n){
 /*4.Implemente la función **int is_final(Node * n)**. Esta función retorna 1 si el nodo corresponde a un nodo final (es decir, todos los valores de la matriz son distintos a 0) y 0 en caso contrario.
 */
 int is_final(Node* n){
+    
     for (int i=0;i<9;i++)
-    { printf("%i\n",n->sudo[i][j]);
+    { 
         for (int j=0;j<0;j++)
         {
-            printf("%i\n",n->sudo[i][j]);
             if (n->sudo[i][j]==0)return 0;
-            printf("%i\n",n->sudo[i][j]);
         }     
     }
     return 1;
