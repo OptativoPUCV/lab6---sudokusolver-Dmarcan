ea08a61bc0b7b8ff362db7e877ae5fd2cbc0af4e
Replit user <>
2023-05-05 15:57:12 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -42,36 +42,6 @@ void print_node(Node* n){
     }
     printf("\n");
 }
-/*
-Cree la función **int is_valid(Node * n)**, para validar si un estado/nodo es válido (cumple con las restricciones del problema). Debe validar que:
-
-- No se repitan números en las filas
-- No se repitan números en las columnas
-- No se repitan números en las submatrices de 3x3
-
-Si el estado es válido la función retorna 1, si no lo es retorna 0.
-
-> Para marcar los números que vayan apareciendo en una fila/columna/submatriz puede usar un arreglo de enteros de largo 10 inicializado con 0s. Cada vez que aparezca un número i, verifique que la casilla i del arreglo sea igual a 0, luego márquela con un '1'. Si la casilla es '1' quiere decir que el número ya estaba marcado por lo que la fla/columna/submatriz no es válida.
-
-----
-**¿Cómo recorrer las submatrices de 3x3?**
-El siguiente código se puede usar para recorrer los elementos de una sumbatriz k (por ejemplo k=4):
-
-    int k=4,p; 
-    for(p=0;p<9;p++){
-        int i=3*(k/3) + (p/3) ;
-        int j=3*(k%3) + (p%3) ;
-        printf("%d ",nodo->sudo[i][j]);
-        if(p%3 == 2) printf("\n");
-    }
-
-La salida del código con el estado de la figura es (submatriz del centro):
-    
-    0 6 0
-    8 0 3
-    0 2 0
-
------*/
 
 int is_valid(Node* n){
     size_t i,j;
@@ -102,23 +72,6 @@ int is_valid(Node* n){
     }
     return 1;
 }
-/*
-1.Cree una función que a partir de un nodo genere una **lista con los nodos adyacentes**:
-
-    List* get_adj_nodes(Node* n){
-       List* list=createList();
-       
-       obtenga los nodos adyacentes a n
-       y agréguelos a la lista
-       
-       return list;
-    }
-
-Recuerde que los nodos adyacentes son generados aplicando las acciones al estado actual.
-
-> - Para el caso del ejemplo, la función debería retornar una lista con **9 nodos**. Cada uno de ellos idéntico al nodo original pero cambiando el valor de la primera casilla vacía, es decir: sudo[0][2], por 1, 2, ..., 9.
-> - Utilice la función Node* copy(Node* n) para copiar nodos.*/
-//Modifique la función *get_adj_nodes* para que sólo los nodos válidos sean retornados (use //la función *is_valid*).
 
 List* get_adj_nodes(Node* n){
     List* list=createList();
@@ -140,13 +93,11 @@ List* get_adj_nodes(Node* n){
                 return list;
             }
         }
-        
     }
     return list;
     
 }
-/*4.Implemente la función **int is_final(Node * n)**. Esta función retorna 1 si el nodo corresponde a un nodo final (es decir, todos los valores de la matriz son distintos a 0) y 0 en caso contrario.
-*/
+
 int is_final(Node* n){
     for (size_t i=0;i<9;i++)
     {
@@ -157,25 +108,6 @@ int is_final(Node* n){
     }
     return 1;
 }
-/*5..Implemente la función Node* DFS(Node* n, int* cont). Esta función realiza una búsqueda en profundidad a partir del nodo n. El algoritmo es el siguiente:
-
-1. Cree un stack S (pila) e inserte el nodo.
-2. Mientras el stack S no se encuentre vacío:
-
-   a) Saque y elimine el primer nodo de S.
-   
-   b) Verifique si corresponde a un estado final, si es así retorne el nodo.
-   
-   c) Obtenga la lista de nodos adyacentes al nodo.
-   
-   d) Agregue los nodos de la lista (uno por uno) al stack S.
-   
-   e) Libere la memoria usada por el nodo.
-   
-3. Si terminó de recorre el grafo sin encontrar una solución, retorne NULL.
-
-Almacene en la variable cont, la cantidad de iteraciones que realiza el algoritmo.
-*/
 
 Node* DFS(Node* initial, int* cont){
     Stack* pila=createStack();
@@ -184,10 +116,7 @@ Node* DFS(Node* initial, int* cont){
     {
         Node* current=top(pila);
         pop(pila);
-        if(is_final(current)==1)
-        {
-            return current;
-        }
+        if(is_final(current)==1)return current;
         List* lista=get_adj_nodes(current);
         for(Node*  primero=first(lista);primero!=NULL;primero=next(lista))
             push(pila,primero);
