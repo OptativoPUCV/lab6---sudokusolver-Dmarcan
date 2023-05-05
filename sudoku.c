bfacd262344e1c90863e53f4a24ef4528f96aa0e
Replit user <>
2023-05-05 03:06:37 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -152,6 +152,7 @@ int is_final(Node* n){
     { 
         for (int j=0;j<0;j++)
         {
+            printf("%i",n->sudo[i][j]);
             if (n->sudo[i][j]==0)return 0;
         }     
     }
