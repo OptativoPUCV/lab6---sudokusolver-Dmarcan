509a0ba7eea0d39d5e4744e878344b4f922110e6
Replit user <>
2023-05-05 02:57:39 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -124,7 +124,7 @@ Recuerde que los nodos adyacentes son generados aplicando las acciones al estado
 List* get_adj_nodes(Node* n){
     List* list=createList();
     size_t i,j;
-    
+    /*
     for (i=0;i < 9;i++)
     {
         for(j=0;j<9;j++)
@@ -147,8 +147,8 @@ List* get_adj_nodes(Node* n){
         }
         
     }
+    */
     
-    /*
     for (i=0;i < 9;i++)
     {
         //printf("%zd %zd\n",i,j);
@@ -160,7 +160,7 @@ List* get_adj_nodes(Node* n){
     {
         Node* nodo=copy(n);
         nodo->sudo[i][j]=k;
-        if(is_valid(n))
+        if(is_valid(nodo))
         {
             //Node* nodo=copy(n);
             //nodo->sudo[i][j]=k;
@@ -168,7 +168,7 @@ List* get_adj_nodes(Node* n){
         }
         else free(nodo);
             
-    }*/
+    }
     return list;
     
 }
